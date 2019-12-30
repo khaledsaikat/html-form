00ee38d155e7a9d6f5d3ef074872cfa154183410
Khaled Hossain <khaled.hossain@daimler.com>
2019-12-30 20:46:34 +0100

    Update readme

--- tests/EscapeTest.php
+++ tests/EscapeTest.php
@@ -4,6 +4,37 @@ namespace UserMeta\Html;
 class EscapeTest extends \TestCase
 {
 
+    public function testValueEscape()
+    {
+        $data = Html::text('hello & world');
+        $this->assertRegExp('/value="hello &amp; world"/', $data);
+
+        $data = Html::text('"hello" & "world" <br>');
+        $this->assertRegExp('/value="&quot;hello&quot; &amp; &quot;world&quot; &lt;br&gt;"/', $data);
+    }
+
+    // @todo Find example of multiple value and test it.
+    public function testMultipleValueEscape()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testHtmlIdEscape()
+    {
+        $data = Html::text(null, [
+            'id' => 'hello&world'
+        ]);
+        $this->assertRegExp('/id="hello&amp;world"/', $data);
+    }
+
+    public function testHtmlClassEscape()
+    {
+        $data = Html::text(null, [
+            'class' => 'hello&world'
+        ]);
+        $this->assertRegExp('/class="hello&amp;world"/', $data);
+    }
+
     public function testTextEscape()
     {
         $data = Html::text('hello & world');
@@ -37,34 +68,26 @@ class EscapeTest extends \TestCase
         $this->assertRegExp('/class="hello&amp;world"/', $data);
     }
 
-    public function testValueEscape()
+    public function testCheckboxEscape()
     {
-        $data = Html::text('hello & world');
-        $this->assertRegExp('/value="hello &amp; world"/', $data);
-
-        $data = Html::text('"hello" & "world" <br>');
-        $this->assertRegExp('/value="&quot;hello&quot; &amp; &quot;world&quot; &lt;br&gt;"/', $data);
-    }
-
-    // @todo Find example of multiple value and test it.
-    public function testMultipleValueEscape()
-    {
-        $this->assertTrue(true);
-    }
-
-    public function testHtmlIdEscape()
-    {
-        $data = Html::text(null, [
-            'id' => 'hello&world'
-        ]);
-        $this->assertRegExp('/id="hello&amp;world"/', $data);
-    }
-
-    public function testHtmlClassEscape()
-    {
-        $data = Html::text(null, [
+        $data = Html::checkbox(null, [
+            'value' => 'hello & world',
             'class' => 'hello&world'
         ]);
+        $this->assertRegExp('/value="hello &amp; world"/', $data);
         $this->assertRegExp('/class="hello&amp;world"/', $data);
+    }
+
+    public function testMultipleCheckboxEscape()
+    {
+        $data = Html::checkbox(null, [
+            'class' => 'hello'
+        ], [
+            'dog' => 'Dog',
+            'cat' => 'Cat'
+        ]);
+        $this->assertRegExp('/value="dog"/', $data);
+        $this->assertRegExp('/value="cat"/', $data);
+        $this->assertRegExp('/class="hello"/', $data);
     }
 }
